aeea7d563b1c93256f52122904934cbf0d93c652
root <you@example.com>
2022-12-11 14:50:06 -0800

    kj

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '1'; c <= '9'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
