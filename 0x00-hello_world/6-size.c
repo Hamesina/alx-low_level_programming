159c064b56a545d6926cf09ff38c92dd9c43b7da
root <you@example.com>
2022-12-11 11:52:42 -0800

    j

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %i byte(S)\n", sizeof(float));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
