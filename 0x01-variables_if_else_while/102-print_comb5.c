e124b0500082cae15af27ad30bca83dc819bf711
root <you@example.com>
2022-12-11 17:41:09 -0800

    l

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0;
+ */
+int main(void)
+{
+	int k;
+
+	int l;
+
+	int i;
+
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for(j = i; j <= 9; j++)
+		{
+			if (i != 9 && j != 9)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(' ');
+			}
+			for (k = 0; k <= 9; k++)
+			{
+				for(l = k + 1; l <= 9; l++)
+				{
+					putchar(k + '0');
+					putchar(l + '0');
+					if ( i != 9 && j != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
