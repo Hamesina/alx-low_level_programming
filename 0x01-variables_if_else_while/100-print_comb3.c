5971b0a26b626530751ba5c73548da73df245916
root <you@example.com>
2022-12-11 16:18:02 -0800

    lk

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,13 @@ int main(void)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
-			putchar(i);
-			putchar(j);
-		}
-		if (i != 8 && j != 9)
-		{
-			putchar(',');
-			putchar(' ');
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 && j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
