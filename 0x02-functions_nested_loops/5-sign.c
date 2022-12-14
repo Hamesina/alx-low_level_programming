eecb3fbbf51eb8d66b11c9791a97df2e5f004a1d
root <you@example.com>
2022-12-14 15:08:48 -0800

    lk

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - is it positive or is it not
+ * @n: the number to be evaluated
+ *
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
