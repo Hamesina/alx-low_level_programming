e9f86ed714bd32b0f162d088dd9390fd9911bbdc
root <you@example.com>
2022-12-17 08:51:08 -0800

    LK

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,15 +7,18 @@ void jack_bauer(void)
 {
 	int hour;
 
-	int minute = 0;
+	int minute;
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		_putchar((hour / 10) + '0');
-		_putchar((hour % 10) + '0');
-		_putchar(':');
-		_putchar((minute / 10) + '0');
-		_putchar((minute % 10) + '0');
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }
