834aa6fef8827558c961fbe4e99c95b53465ea11
root <you@example.com>
2022-12-18 11:29:30 -0800

    is it uppercase? - take 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - check if it is uppercase
+ * @c: number
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
