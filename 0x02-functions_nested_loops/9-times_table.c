40d2cff1fbd45d042e40256d24c3ec84c2467f58
root <you@example.com>
2022-12-18 01:56:47 -0800

    lk

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,19 +7,21 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		for (j = 0; j <= 9; j++)
 		{
+			_putchar(',');
+			_putchar(' ');
 			k = i * j;
-			_putchar(k + '0');
-			if (j != 9)
-			{
-				_putchar(',');
+			if (k <= 9)
 				_putchar(' ');
-			}
+			else
+				_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
 
