e428144ec8b0b82fdf9f4b15f9719c3b9bec50d8
root <you@example.com>
2022-12-19 13:45:10 -0800

    task3 take 1

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - put into stdout
+ * Return: void
+ * @str: string
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
