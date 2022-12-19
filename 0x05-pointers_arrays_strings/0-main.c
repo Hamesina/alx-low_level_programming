78e21c6e384ed4173f6b45019e9fec5ea80ebf03
root <you@example.com>
2022-12-19 12:59:22 -0800

    task 1 take 1

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry, check the code
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
