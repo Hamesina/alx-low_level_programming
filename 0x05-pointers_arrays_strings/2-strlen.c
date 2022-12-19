f6d88acf0cc0690a77c6b4c85b6ee854c7339bd4
root <you@example.com>
2022-12-19 13:29:21 -0800

    task2 take 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - calculates the length of the string
+ * @s: string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
