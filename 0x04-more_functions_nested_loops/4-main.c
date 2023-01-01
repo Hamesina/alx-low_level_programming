c04ae0ca312ea81f1fb2fc0af1b92a6b95d1d979
root <you@example.com>
2023-01-01 06:13:31 -0800

    task 4 take 1

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - print them numbers except 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
