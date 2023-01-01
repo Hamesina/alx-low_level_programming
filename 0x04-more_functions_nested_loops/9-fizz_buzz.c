fce2f5d8c52d72a005fe96347de3aa730613b46c
root <you@example.com>
2023-01-01 11:45:06 -0800

    task 9 take 2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,12 +9,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf(" Fizz");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf(" FizzBuzz");
 		else if (i % 5 == 0)
 			printf(" Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf(" FizzBuzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
 		else
 			printf(" %d", i);
 	}
