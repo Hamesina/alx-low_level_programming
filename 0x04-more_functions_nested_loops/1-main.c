0caa381f9dfbe27c9a8be77cb01ea6e5ecf261fe
root <you@example.com>
2023-01-01 05:17:43 -0800

    ask 1 take 0

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isdigit(c));
+	return (0);
+}
