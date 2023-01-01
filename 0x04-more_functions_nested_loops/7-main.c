5f42fd2653751b25195d59a99f467e9101ea379d
root <you@example.com>
2023-01-01 09:58:55 -0800

    task 7 take 1

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	print_diagonal(-4);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - prints adiagonal
+ * @n: integer
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n < 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
