fd8b9f01b1424b43727868a4d0fc6902cd8dd653
root <you@example.com>
2023-01-01 05:37:19 -0800

    task2 take1

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplied number
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
