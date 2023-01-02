83ba0b4e8f10360afbe6a771b5225e6aafa5c6a8
root <you@example.com>
2023-01-02 15:05:12 -0800

    task 5 take 1

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - lowercse letters in the string to upper case
+ * @str: string
+ * Return: the string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
